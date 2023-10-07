b5cd204392362e42ad91de2c7ec5ac9a92ed42f4
tieuuu <phamhuytrieu12092003@gmail.com>
2023-10-07 13:30:29 +0700

    create modal CRUD

--- src/main/resources/static/assets/js/adminRoute.js
+++ src/main/resources/static/assets/js/adminRoute.js
@@ -5,14 +5,40 @@ app.config(function ($routeProvider) {
             templateUrl: "/pages/dashboard.html",
             controller: "dashboardController"
         })
+        //game management
         .when("/game", {
             templateUrl: "/pages/gameList.html",
             controller: "gameController"
         })
+        //order management
         .when("/order", {
             templateUrl: "/pages/orderList.html",
             controller: "oderController"
         })
+        //user management
+        .when("/user", {
+            templateUrl: "/pages/profile.html",
+            controller: "profileController"
+        })
+        //review management
+        .when("/review", {
+            templateUrl: "/pages/profile.html",
+            controller: "reviewController"
+        })
+        //queue management
+        .when("/queue", {
+            templateUrl: "/pages/profile.html",
+            controller: "queueController"
+        })
+        //voucher management
+        .when("/voucher", {
+            templateUrl: "/pages/profile.html",
+            controller: "voucherController"
+        })
+        // .when("/sign-in", {
+        //     templateUrl: "/sign-in.html",
+        //     controller: "signInController"
+        // })
         .otherwise({
             redirectTo: "/"
         });
