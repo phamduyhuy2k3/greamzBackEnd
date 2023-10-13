5d7c471865e1a2d72dd8b41a32939f6233b78027
Dinh Quy <quyvdps24908@fpt.edu.vn>
2023-10-13 17:03:53 +0700

    gamecontrollerr

--- src/main/resources/static/assets/js/adminController.js
+++ src/main/resources/static/assets/js/adminController.js
@@ -1,2 +1,15 @@
 app.controller("mainController", function ($scope, $routeParams) {
 });
+
+app.controller("gameController", function ($scope, $http, $document) {
+    $scope.games = [];
+    $scope.initialize = function () {
+        $http.get("/api/game/findALl").then(resp => {
+            $scope.games = resp.data;
+            console.log($scope.games)
+        })
+            .catch(error => {
+                console.log("Error", error);
+            });
+    }
+})
