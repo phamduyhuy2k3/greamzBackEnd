32675d5430b10a5360629f057e1e7a0a313ed53e
Dinh Quy <quyvdps24908@fpt.edu.vn>
2023-11-07 14:04:32 +0700

    a

--- src/main/resources/static/assets/js/categoryController.js
+++ src/main/resources/static/assets/js/categoryController.js
@@ -7,56 +7,39 @@ app.controller("categoryController", function ($scope, $http, $document, $cookie
     $scope.imageCloudinary;
 
 
-        $scope.form = {
-            id: '',
-            name: '',
-            description: '',
-            image: '',
-            categoryTypes: '',
-        }
-        $scope.pager = {
-            toFirst() {
-                this.number = 0;
-                this.fetchPage();
-            },
-            toLast() {
-                this.number = this.totalPages - 1
-                this.fetchPage();
-
-            },
-            next() {
-                this.number++;
-                if (this.number >= this.totalPages) {
-                    this.number = 0;
-                }
-                this.fetchPage();
-            },
-            prev() {
-                this.number--;
-                if (this.number < 0) {
-                    this.number = this.totalPages - 1;
-                }
-                this.fetchPage();
-            },
-            fetchPage() {
-                $http.get(`/api/v1/category/findAllPagination?page=${this.number}&size=10`, {
-                    headers: {
-                        'Authorization': 'Bearer ' + $cookies.get('accessToken')
-                    }
-                }).then(resp => {
-                    $scope.pager = {
-                        ...$scope.pager,
-                        ...resp.data
-                    };
-                })
-            }
-
+    $scope.form = {
+        id: '',
+        name: '',
+        description: '',
+        image: '',
+        categoryTypes: '',
     }
-    $scope.searchCategoryEvent = function () {
-        if ($scope.searchCategory === '') {
-            $scope.pager.fetchPage();
-        } else {
-            $http.get(`/api/v1/category/search?term=${$scope.searchCategory}`, {
+    $scope.pager = {
+        toFirst() {
+            this.number = 0;
+            this.fetchPage();
+        },
+        toLast() {
+            this.number = this.totalPages - 1
+            this.fetchPage();
+
+        },
+        next() {
+            this.number++;
+            if (this.number >= this.totalPages) {
+                this.number = 0;
+            }
+            this.fetchPage();
+        },
+        prev() {
+            this.number--;
+            if (this.number < 0) {
+                this.number = this.totalPages - 1;
+            }
+            this.fetchPage();
+        },
+        fetchPage() {
+            $http.get(`/api/v1/category/findAllPagination?page=${this.number}&size=10`, {
                 headers: {
                     'Authorization': 'Bearer ' + $cookies.get('accessToken')
                 }
@@ -67,6 +50,7 @@ app.controller("categoryController", function ($scope, $http, $document, $cookie
                 };
             })
         }
+
     }
     //Image
     $scope.imageCloudinary = cloudinary.createMediaLibrary(
@@ -110,125 +94,97 @@ app.controller("categoryController", function ($scope, $http, $document, $cookie
     });
 
 
-        $scope.initialize = function () {
+    $scope.initialize = function () {
 
-            $http.get("/api/v1/category/findAllPagination", {
-                headers: {
-                    "Authorization": "Bearer " + $cookies.get("accessToken")
-                }
-            }).then(
-                resp => {
-                    $scope.pager = {
-                        ...$scope.pager,
-                        ...resp.data
-                    };
-                },
-                error => {
-                    console.log("Error", error);
-                }
-            );
+        $http.get("/api/v1/category/findAllPagination", {
+            headers: {
+                "Authorization": "Bearer " + $cookies.get("accessToken")
+            }
+        }).then(
+            resp => {
+                $scope.pager = {
+                    ...$scope.pager,
+                    ...resp.data
+                };
+            },
+            error => {
+                console.log("Error", error);
+            }
+        );
 
-            $http.get("/api/v1/category/types", {
-                headers: {
-                    "Authorization": "Bearer " + $cookies.get("accessToken")
-                }
-            }).then(
-                resp => {
-                    $scope.categoryTypes = resp.data;
-                    console.log($scope.categoryTypes); // Kiểm tra giá trị của categoryTypes
-                },
-                error => {
-                    console.log("Error", error);
-                }
-            );
-        }
+        $http.get("/api/v1/category/types", {
+            headers: {
+                "Authorization": "Bearer " + $cookies.get("accessToken")
+            }
+        }).then(
+            resp => {
+                $scope.categoryTypes = resp.data;
+                console.log($scope.categoryTypes); // Kiểm tra giá trị của categoryTypes
+            },
+            error => {
+                console.log("Error", error);
+            }
+        );
+    }
 
-        $scope.deleteCategoryImage = function () {
-            $scope.form.image = $scope.form.image.replace($scope.form.image, '');
-            console.log($scope.form.image)
-        }
+    $scope.deleteCategoryImage = function () {
+        $scope.form.image = $scope.form.image.replace($scope.form.image, '');
+        console.log($scope.form.image)
+    }
 
-        $scope.delete = function (id) {
-            Swal.fire({
-                title: "Are you sure?",
-                text: "You won't be able to revert this!",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Yes, delete it!"
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    if (id) {
-                        $http.delete(`/api/v1/category/delete/${id}`, {
-                            headers: {
-                                "Authorization": "Bearer " + $cookies.get("accessToken")
-                            }
-                        }).then(resp => {
-                            Swal.fire({
-                                title: "Deleted!",
-                                text: "Deleted the category successfully!",
-                                icon: "success"
-                            });
-                            $scope.initialize();
-                        }).catch(error => {
-                            Swal.fire({
-                                icon: "error",
-                                title: "Oops...",
-                                text: "Error deleting category!",
-                            });
-                            console.log("Error", error);
-                        });
+    $scope.delete = function (id) {
+        if (confirm("Do you want to delete this category?")) {
+            if (id) {
+                $http.delete(`/api/v1/category/delete/${id}`, {
+                    headers: {
+                        "Authorization": "Bearer " + $cookies.get("accessToken")
                     }
-                }
-            });
-        }
-
-
-        $scope.reset = function () {
-            $scope.form = {
-                id: '',
-                name: '',
-                description: '',
-                image: '',
-                categoryTypes: '',
-            };
-            $scope.action = 'create';
-            $scope.initialize();
-        }
-
-        $scope.create = function () {
-            // Gán giá trị type từ selectedCategoryType vào form
-            $http.post("/api/v1/category/save", $scope.form, {
-                headers: {
-                    "Authorization": "Bearer " + $cookies.get("accessToken"),
-                    "Content-Type": "application/json"
-                },
-            }).then(
-                resp => {
-                    Swal.fire({
-                        position: "center",
-                        icon: "success",
-                        title: "Saved the category successfully!",
-                        showConfirmButton: false,
-                        timer: 1500
-                    });
-                    $scope.reset();
-                },
-                error => {
-                    Swal.fire({
-                        icon: "error",
-                        title: "Oops...",
-                        text: "Error saving category!",
-                    });
+                }).then(resp => {
+                    alert("Deleted successfully!");
+                    $scope.initialize();
+                }).catch(error => {
+                    alert("Error deleting category!");
                     console.log("Error", error);
-                }
-            )
+                });
+            } else {
+                alert("Error deleting category!");
+            }
         }
-        $scope.edit = function (id) {
-            $scope.form = $scope.categories.find(value => value.id === id);
-        }
+    }
 
+
+    $scope.reset = function () {
+        $scope.form = {
+            id: '',
+            name: '',
+            description: '',
+            image: '',
+            categoryTypes: '',
+        };
+        $scope.action = 'create';
         $scope.initialize();
     }
-)
+
+    $scope.create = function () {
+        // Gán giá trị type từ selectedCategoryType vào form
+        $http.post("/api/v1/category/save", $scope.form, {
+            headers: {
+                "Authorization": "Bearer " + $cookies.get("accessToken"),
+                "Content-Type": "application/json"
+            },
+        }).then(
+            resp => {
+                alert("Saved successfully!");
+                $scope.reset();
+            },
+            error => {
+                console.log("Error", error);
+            }
+        )
+    }
+    $scope.edit = function (id) {
+        $scope.form = $scope.categories.find(value => value.id === id);
+    }
+
+    $scope.initialize();
+})
